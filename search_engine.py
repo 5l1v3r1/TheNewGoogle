e69e7b176952dcaf812df5cc68cad3bd14ac14c8
Svetlin Slavov <sslavov93@gmail.com>
2014-11-25 17:45:04 +0200

    Crawler fully working, rating system and actual search engine left.

--- search_engine.py
+++ search_engine.py
@@ -2,7 +2,7 @@ import crawler
 from create_pages import Page
 from create_website import Website
 from create_base import Base
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, update
 from sqlalchemy.orm import Session
 
 
@@ -13,11 +13,22 @@ def from_file_to_list(file):
     return websites
 
 
+def get_domain(url):
+    url = url.strip("http://www.")
+    return url
+
+
 def fill_database(session, titles, scanned_urls):
-    session.add(Website(title=titles[0], url=scanned_urls[0]))
+    site_domain = get_domain(scanned_urls[0])
+    session.add(Website(title=titles[0], url=scanned_urls[0], domain=site_domain))
     ID = session.query(Website.id).filter(Website.title == titles[0]).one()
+    ID = ID[0]
     for count in range(1, len(titles)):
-        session.add(Page(title=titles[count], url=scanned_urls[count], website_id=ID[0]))
+        session.add(Page(title=titles[count], url=scanned_urls[count], website_id=ID))
+    p_count = session.query(Page).filter(Website.id == ID).count()
+    p_count += 1  # Main page including
+    session.execute(update(Website).where(Website.id == ID).values(pages_count=p_count))
+    session.commit()
 
 
 def crawl(session, websites, titles, urls):
